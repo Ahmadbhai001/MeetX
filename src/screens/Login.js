8db62c4e80edec33013344cbfc047cef88aec958
Hafiz Ahmad <ahmadrafique1997@gmail.com>
2023-03-06 18:08:03 +0500

    KeyboadAvoiding

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,19 +1,21 @@
-import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, Image, KeyboardAvoidingView} from 'react-native';
 import React, {useState} from 'react';
 import {TextInput, Divider} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 const Login = ({navigation}) => {
   const [emailfocus, setEmailfocus] = useState(false);
   const [passwordfocus, setPasswordfocus] = useState(false);
   return (
-    <View style={styles.container}>
+    
+    <KeyboardAvoidingView behavior="height" style={styles.container}>
       <Text
         style={{
           fontSize: 35,
           color: '#ffffff',
           fontWeight: 'bold',
-          marginBottom: 20,
+          marginBottom: 10,
         }}>
         Log In
       </Text>
@@ -67,20 +69,20 @@ const Login = ({navigation}) => {
       <Text style={styles.or}>OR</Text>
       <Text style={styles.Sinwith}>Sign in with</Text>
 
-      {/* Icons */}
-      <View>
+      {/* FaceBook,Google Buttons */}
+      <View style={{}}>
         <TouchableOpacity style={styles.facebookBtn}>
           <Icon
             name="facebook"
             // iconColor="white"
-            color='#ffffff'
+            color="#ffffff"
             size={30}
-            style={{paddingHorizontal: 10, paddingVertical: 5}}
+            style={{paddingHorizontal: 5, padding: 5}}
           />
           <Text
             style={{
               fontSize: 20,
-              paddingRight: 10,
+              //  paddingRight: 40,
               fontWeight: '800',
               color: '#ffffff',
             }}>
@@ -92,30 +94,36 @@ const Login = ({navigation}) => {
             name="google"
             color="#1c6897"
             size={30}
-            style={{paddingHorizontal: 10, paddingVertical: 5}}
+            style={{paddingHorizontal: 5, padding: 5}}
           />
           <Text
             style={{
               fontSize: 20,
-              paddingRight: 10,
+
               color: '#1c6897',
               fontWeight: '800',
             }}>
             Sign with Google
           </Text>
         </TouchableOpacity>
+        {/* FaceBook,Google Buttons */}
       </View>
+      {/* Divider Start */}
       <Divider style={{width: '80%', marginTop: 20}} />
+      {/* Divider End*/}
       <View>
-        <Text style={{fontSize: 15, fontWeight: '500',}}>
-          Dont's have an account 
-          <Text style={{fontSize: 18, fontWeight: '800', color: '#ffffff', }} onPress={()=>navigation.navigate('Signup')} >
-          {' '}{' '}
+        <Text style={{fontSize: 15, fontWeight: '500'}}>
+          Dont's have an account
+          <Text
+            style={{fontSize: 18, fontWeight: '800', color: '#ffffff'}}
+            onPress={() => navigation.navigate('Signup')}>
+            {' '}
             Sign Up
           </Text>
         </Text>
       </View>
-    </View>
+    </KeyboardAvoidingView>
+    
   );
 };
 
@@ -123,14 +131,18 @@ export default Login;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    alignContent: 'center',
+paddingTop:5,
     backgroundColor: '#0c2c41',
+  
   },
   textInputContainer: {
     width: '90%',
-    margin: 10,
+    //  margin: 10,
+    padding: 5,
   },
   textinput: {
     // Radius: 20,
@@ -138,7 +150,7 @@ const styles = StyleSheet.create({
   btn: {
     marginTop: 10,
     justifyContent: 'center',
-    width: '100%',
+    width: '80%',
   },
   btnTex: {
     backgroundColor: '#ffffff',
@@ -149,23 +161,24 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 30,
     fontWeight: 'bold',
-    paddingVertical: 10,
+    padding: 10,
   },
   forgot: {
     fontSize: 20,
     color: '#ffffff',
     marginTop: 15,
   },
+
   or: {
     fontSize: 20,
     color: '#ffffff',
     fontWeight: 'bold',
-    margin: 10,
+    margin: 1,
   },
+
   Sinwith: {
     fontSize: 25,
     color: '#ffffff',
-    fontWeight: 'bold',
   },
   facebookBtn: {
     flexDirection: 'row',
@@ -180,6 +193,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#e6ffe6',
     borderRadius: 10,
-    marginTop: 10,
+    marginTop: 5,
   },
 });
